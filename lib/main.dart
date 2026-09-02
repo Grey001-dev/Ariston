d26917a828177562fdc39a03555e051e3af3903c
Tosin Folarin <folarintosin2007@gmail.com>
2026-09-02 01:42:43 +0100

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,12 @@
+import 'package:ariston/services/auth_service.dart';
 import 'package:ariston/sign_in_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: ".env");
+  await AuthService().initGoogleSignIn();
   runApp(const MyApp());
 }
 
